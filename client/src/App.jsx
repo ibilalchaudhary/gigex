35a1843a060b5b369772b3f6f66033f4106961ba
singhjyotiraditya <adsi123qwe@gmail.com>
2024-01-31 05:19:58 +0530

    Added Footer to Gigex

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
-
 
 
 function App() {
 
-  const Layout = () => {
-    return(
-      <div className="app">
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
-      </div>
-    )
-  }
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    },
-  ]);
-
   return (
     <div>
-      <RouterProvider router={router} />
+      <Navbar />
     </div>
   )
 }
