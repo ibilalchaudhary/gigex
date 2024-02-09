fc4b0d6429dc0f8682f022d4dafd1fbce44987e1
singhjyotiraditya <adsi123qwe@gmail.com>
2024-02-10 01:10:22 +0530

    Added a Gig Review System

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -4,7 +4,7 @@ import { Slider } from "infinite-react-carousel/lib";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
-// import Reviews from "../../components/reviews/Reviews";
+import Reviews from "../../components/reviews/Reviews";
 
 function Gig() {
   const { id } = useParams();
@@ -130,7 +130,7 @@ function Gig() {
                 </div>
               </div>
             )}
-            {/* <Reviews gigId={id} /> */}
+            <Reviews gigId={id} />
           </div>
           <div className="right">
             <div className="price">
