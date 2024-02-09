bd05413d776510b20224bfee840cfe0c9e1fe371
singhjyotiraditya <adsi123qwe@gmail.com>
2024-02-10 04:30:27 +0530

    Solving Error for Deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import html from "@rollup/plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    html({
-      inject: {
-        injectTo: "head",
-        tag: '<link rel="icon" type="image/png" href="/client/public/favicon.ico" />',
-      },
-    }),
+    react()
   ],
 });
